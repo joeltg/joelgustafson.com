48e03a71fbc521f0ae5b14cbf0b49b35e963aa50
Joel Gustafson <joelg@mit.edu>
2024-08-21 11:01:27 -0400

    upgrade react-lezer-highlighter and next

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -1,73 +1,73 @@
-import React from "react"
-import ReactMarkdown from "react-markdown"
-import rehypeRaw from "rehype-raw"
-import { slug } from "github-slugger"
-import { Code } from "react-lezer-highlighter"
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import { slug } from "github-slugger";
+import { Code } from "react-lezer-highlighter";
 
 const flatten = (text: string, child: React.ReactNode): string => {
-	if (typeof child === "string") {
-		return text + child
-	} else if (React.isValidElement(child)) {
-		return React.Children.toArray(child.props.children).reduce(flatten, text)
-	} else {
-		return text
-	}
-}
+  if (typeof child === "string") {
+    return text + child;
+  } else if (React.isValidElement(child)) {
+    return React.Children.toArray(child.props.children).reduce(flatten, text);
+  } else {
+    return text;
+  }
+};
 
 const Anchor = (props: { id: string; children: React.ReactNode[] }) => (
-	<a href={`#${props.id}`}>
-		<span className="anchor">#</span>
-		<span>{props.children}</span>
-	</a>
-)
+  <a href={`#${props.id}`}>
+    <span className="anchor">#</span>
+    <span>{props.children}</span>
+  </a>
+);
 
-const components = {
-	code(props: {
-		inline?: boolean
-		className?: string
-		children: React.ReactNode[]
-	}) {
-		if (props.inline) {
-			return <code>{props.children}</code>
-		} else {
-			const source = String(props.children).replace(/\n+$/, "")
-			return <Code language={props.className} source={source} />
-		}
-	},
-	img(props: { src?: string; alt?: string }) {
-		return <img srcSet={`${props.src} 2x`} {...props} />
-	},
-	h1: (props: { children: React.ReactNode[] }) => {
-		return <h1>{props.children}</h1>
-	},
-	h2: (props: { children: React.ReactNode[] }) => {
-		const children = React.Children.toArray(props.children)
-		const id = slug(children.reduce(flatten, ""))
-		return (
-			<h2 id={id}>
-				<Anchor id={id}>{props.children}</Anchor>
-			</h2>
-		)
-	},
-	h3: (props: { children: React.ReactNode[] }) => {
-		const children = React.Children.toArray(props.children)
-		const id = slug(children.reduce(flatten, ""))
-		return (
-			<h3 id={id}>
-				<Anchor id={id}>{props.children}</Anchor>
-			</h3>
-		)
-	},
+interface CodeProps {
+  inline?: boolean;
+  className?: string;
+  children: React.ReactNode[];
 }
 
+const components = {
+  code(props: CodeProps) {
+    if (props.inline) {
+      return <code>{props.children}</code>;
+    } else {
+      const source = String(props.children).replace(/\n+$/, "");
+      return <Code language={props.className} source={source} />;
+    }
+  },
+  img(props: { src?: string; alt?: string }) {
+    return <img srcSet={`${props.src} 2x`} {...props} />;
+  },
+  h1: (props: { children: React.ReactNode[] }) => {
+    return <h1>{props.children}</h1>;
+  },
+  h2: (props: { children: React.ReactNode[] }) => {
+    const children = React.Children.toArray(props.children);
+    const id = slug(children.reduce(flatten, ""));
+    return (
+      <h2 id={id}>
+        <Anchor id={id}>{props.children}</Anchor>
+      </h2>
+    );
+  },
+  h3: (props: { children: React.ReactNode[] }) => {
+    const children = React.Children.toArray(props.children);
+    const id = slug(children.reduce(flatten, ""));
+    return (
+      <h3 id={id}>
+        <Anchor id={id}>{props.children}</Anchor>
+      </h3>
+    );
+  },
+};
+
 export interface MarkdownProps {
-	source: string
+  source: string;
 }
 
 export default function Markdown(props: MarkdownProps) {
-	return (
-		<ReactMarkdown rehypePlugins={[rehypeRaw]} components={components}>
-			{props.source}
-		</ReactMarkdown>
-	)
+  return (
+    // <ReactMarkdown rehypePlugins={[rehypeRaw]} components={components}>
+    <ReactMarkdown components={components}>{props.source}</ReactMarkdown>
+  );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,75 +1,75 @@
-import React from "react"
-import type { AppProps } from "next/app"
-import Head from "next/head"
+import React from "react";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
-import type { PageProps } from "utils/pages"
+import type { PageProps } from "utils/pages";
 
-import { parser as javascriptParser } from "@lezer/javascript"
-import { parser as lezerParser } from "@lezer/lezer"
+import { parser as javascriptParser } from "@lezer/javascript";
+import { parser as lezerParser } from "@lezer/lezer";
+import { Parsers } from "react-lezer-highlighter";
 
-import "react-lezer-highlighter/styles/default.css"
-import "../style.css"
-import { Parsers } from "react-lezer-highlighter"
+import "react-lezer-highlighter/styles/default.css";
+import "../style.css";
 
 const parsers = {
-	"language-javascript": javascriptParser,
-	"language-js": javascriptParser,
-	"language-jsx": javascriptParser.configure({ dialect: "jsx" }),
-	"language-typescript": javascriptParser.configure({ dialect: "ts" }),
-	"language-ts": javascriptParser.configure({ dialect: "ts" }),
-	"language-tsx": javascriptParser.configure({ dialect: "ts jsx" }),
-	"language-lezer": lezerParser,
-}
+  "language-javascript": javascriptParser,
+  "language-js": javascriptParser,
+  "language-jsx": javascriptParser.configure({ dialect: "jsx" }),
+  "language-typescript": javascriptParser.configure({ dialect: "ts" }),
+  "language-ts": javascriptParser.configure({ dialect: "ts" }),
+  "language-tsx": javascriptParser.configure({ dialect: "ts jsx" }),
+  "language-lezer": lezerParser,
+};
 
 export default function App(props: AppProps<PageProps>) {
-	const { Component, pageProps } = props
-	const title = pageProps.title
-		? `${pageProps.title} | Joel Gustafson`
-		: "Joel Gustafson"
+  const { Component, pageProps } = props;
+  const title = pageProps.title
+    ? `${pageProps.title} | Joel Gustafson`
+    : "Joel Gustafson";
 
-	return (
-		<Parsers.Provider value={parsers}>
-			<Head>
-				<title>{title}</title>
-				<link rel="icon" href="/favicon.png" type="image/png" />
-				<link
-					rel="preload"
-					href="/LyonText-Regular-Web.woff2"
-					as="font"
-					type="font/woff2"
-					crossOrigin="anonymous"
-				/>
-				<link
-					rel="preload"
-					href="/LyonText-RegularItalic-Web.woff2"
-					as="font"
-					type="font/woff2"
-					crossOrigin="anonymous"
-				/>
-				<link
-					rel="preload"
-					href="/LyonText-Semibold-Web.woff2"
-					as="font"
-					type="font/woff2"
-					crossOrigin="anonymous"
-				/>
-				<link
-					rel="preload"
-					href="/LyonText-SemiboldItalic-Web.woff2"
-					as="font"
-					type="font/woff2"
-					crossOrigin="anonymous"
-				/>
+  return (
+    <Parsers.Provider value={parsers}>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link
+          rel="preload"
+          href="/LyonText-Regular-Web.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/LyonText-RegularItalic-Web.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/LyonText-Semibold-Web.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/LyonText-SemiboldItalic-Web.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
 
-				<meta name="twitter:card" content="summary" />
-				<meta name="twitter:site" content="@gustafjt" />
-				<meta name="twitter:title" content={title} />
-			</Head>
-			<main>
-				<article>
-					<Component {...pageProps} />
-				</article>
-			</main>
-		</Parsers.Provider>
-	)
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@gustafjt" />
+        <meta name="twitter:title" content={title} />
+      </Head>
+      <main>
+        <article>
+          <Component {...pageProps} />
+        </article>
+      </main>
+    </Parsers.Provider>
+  );
 }
